03c52a5147a4f274ed24cedb3d92a7ce723869fa
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-04-28 23:32:52 -0700

    Added delete, update and show all tasks, basic REST API completed flask/mysql

--- src/app.py
+++ src/app.py
@@ -65,6 +65,36 @@ def get_tasks():
 
 @app.route('/tasks/<id>', methods=['GET'])
 def get_task(id):
+    task = Task.query.get(id)
+    return task_schema.jsonify(task)
+
+
+@app.route('/tasks/<id>', methods=['PUT'])
+def update_task(id):
+    task = Task.query.get(id)
+
+    title = request.json['title']
+    description = request.json['description']
+
+    task.title = title
+    task.description = description
+
+    db.session.commit()
+    return task_schema.jsonify(task)
+
+
+@app.route('/tasks/<id>', methods=['DELETE'])
+def delete_task(id):
+    task = Task.query.get(id)
+    db.session.delete(task)
+    db.session.commit()
+
+    return task_schema.jsonify(task)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Welcome to my API'})
 
 
 if __name__ == "__main__":
